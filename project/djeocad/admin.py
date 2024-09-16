5063506e39a138473733d9bf38f7e307fe6c3516
andywar65 <andy.war1965@gmail.com>
2024-09-16 21:00:45 +0200

    adding drawing model

--- project/djeocad/admin.py
+++ project/djeocad/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Drawing
+
+@admin.register(Drawing)
+class DrawingAdmin(admin.ModelAdmin):
+    list_display = ("title",)
--- project/djeocad/models.py
+++ project/djeocad/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class Drawing(models.Model):
+
+    title = models.CharField(
+        _("Name"),
+        help_text=_("Name of the drawing"),
+        max_length=50,
+    )
