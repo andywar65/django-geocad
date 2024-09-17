619fe411f33dcea0cf3dba16ecf111e41e2bfee4
andywar65 <andy.war1965@gmail.com>
2024-09-17 19:22:57 +0200

    added more fields to drawing model

--- project/djeocad/models.py
+++ project/djeocad/models.py
@@ -1,5 +1,7 @@
+from django.core.validators import FileExtensionValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from djgeojson.fields import PointField
 
 
 class Drawing(models.Model):
@@ -9,6 +11,51 @@ class Drawing(models.Model):
         help_text=_("Name of the drawing"),
         max_length=50,
     )
+    parent = models.ForeignKey(
+        "self",
+        on_delete=models.SET_NULL,
+        related_name="parent_drawing",
+        verbose_name=_("Parent Drawing"),
+        null=True,
+        blank=True,
+    )
+    image = models.ImageField(
+        _("Image"), upload_to="uploads/djeocad/images/", null=True, blank=True
+    )
+    dxf = models.FileField(
+        _("DXF file"),
+        max_length=200,
+        upload_to="uploads/djeocad/dxf/",
+        validators=[
+            FileExtensionValidator(
+                allowed_extensions=[
+                    "dxf",
+                ]
+            )
+        ],
+    )
+    geom = PointField(_("Location"), null=True, blank=True)
+    designx = models.FloatField(
+        _("Design point X..."),
+        default=0,
+    )
+    designy = models.FloatField(
+        _("...Y"),
+        default=0,
+    )
+    rotation = models.FloatField(
+        _("Rotation"),
+        default=0,
+    )
+    epsg = models.IntegerField(
+        _("CRS code"),
+        null=True,
+        editable=False,
+    )
+
+    class Meta:
+        verbose_name = _("Drawing")
+        verbose_name_plural = _("Drawings")
 
     def __str__(self):
         return self.title
