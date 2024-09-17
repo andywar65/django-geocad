72fa00a8e37c6111e79d9d97885fffc6db4ff9b7
andywar65 <andy.war1965@gmail.com>
2024-09-17 18:28:40 +0200

    moved app back into project

--- project/djeocad/admin.py
+++ project/djeocad/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from .models import Drawing
 
+
 @admin.register(Drawing)
 class DrawingAdmin(admin.ModelAdmin):
     list_display = ("title",)
--- project/djeocad/tests.py
+++ project/djeocad/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 
 from .models import Drawing
 
+
 class GeoCADModelTest(TestCase):
     @classmethod
     def setUpTestData(cls):
