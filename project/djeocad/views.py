a33d1f65c0ee68a56eb726c558d83de85e609b0a
andywar65 <andy.war1965@gmail.com>
2024-09-18 18:59:19 +0200

    adding drawing list view

--- project/djeocad/views.py
+++ project/djeocad/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render  # noqa
+from typing import Any
 
-# Create your views here.
+from django.conf import settings
+from django.db.models.query import QuerySet
+from django.views.generic import ListView
+
+from .models import Drawing
+
+
+class BaseListView(ListView):
+    model = Drawing
+    template_name = "djeocad/drawing_list.html"
+
+    def get_queryset(self) -> QuerySet[Any]:
+        qs = Drawing.objects.exclude(epsg=None)
+        return qs
+
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["unreferenced"] = Drawing.objects.filter(epsg=None)
+        context["leaflet_config"] = settings.LEAFLET_CONFIG
+        return context
