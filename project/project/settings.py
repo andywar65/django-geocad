1f47c79bef9248e7cb4081f3c72c5fa4cae6e5a6
andywar65 <andy.war1965@gmail.com>
2024-09-19 23:01:13 +0200

    extend project base template

--- project/project/settings.py
+++ project/project/settings.py
@@ -69,7 +69,7 @@ ROOT_URLCONF = "project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "project/templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
