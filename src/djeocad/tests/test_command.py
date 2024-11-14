b4719b4a5cd9b09b9b417837d872de2dc1db78f0
andywar65 <andy.war1965@gmail.com>
2024-11-15 00:30:19 +0100

    added command test

--- src/djeocad/tests/test_command.py
+++ src/djeocad/tests/test_command.py
@@ -1,15 +1,17 @@
+from io import StringIO
 from pathlib import Path
 
 from django.conf import settings
-from django.contrib.auth.models import User
 from django.core.files.uploadedfile import SimpleUploadedFile
+from django.core.management import call_command
 from django.test import TestCase, override_settings
 
 from djeocad.models import Drawing, Entity, Layer
 
 
 @override_settings(MEDIA_ROOT=Path(settings.MEDIA_ROOT).joinpath("tests"))
-class GeoCADModelTest(TestCase):
+@override_settings(DEBUG=True)
+class GeoCADCommandTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         dxf_path = Path(settings.BASE_DIR).joinpath(
@@ -21,23 +23,23 @@ class GeoCADModelTest(TestCase):
         draw1.title = "Not referenced"
         draw1.dxf = SimpleUploadedFile("nogeo.dxf", content, "image/x-dxf")
         draw1.save()
-        dxf_path = Path(settings.BASE_DIR).joinpath(
-            "djeocad/static/djeocad/tests/yesgeo.dxf"
+        layer = Layer.objects.create(drawing=draw1, name="Layer")
+        block = Layer.objects.create(drawing=draw1, name="Bloke", is_block=True)
+        Entity.objects.create(
+            layer=layer,
+            geom={
+                "type": "GeometryCollection",
+                "geometries": [
+                    {
+                        "type": "LineString",
+                        "coordinates": [
+                            [[12.523826, 41.90339], [12.523826, 41.903391]]
+                        ],
+                    }
+                ],
+            },
+            data={"Foo": "Bar"},
         )
-        with open(dxf_path, "rb") as f:
-            content = f.read()
-        img_path = Path(settings.BASE_DIR).joinpath(
-            "djeocad/static/djeocad/tests/image.jpg"
-        )
-        with open(img_path, "rb") as fi:
-            img_content = fi.read()
-        draw2 = Drawing()
-        draw2.title = "Referenced"
-        draw2.dxf = SimpleUploadedFile("yesgeo.dxf", content, "image/x-dxf")
-        draw2.image = SimpleUploadedFile("image.jpg", img_content, "image/jpeg")
-        draw2.save()
-        User.objects.create_superuser("boss", "test@example.com", "p4s5w0r6")
-        layer = Layer.objects.create(drawing=draw2, name="Layer")
         Entity.objects.create(
             layer=layer,
             geom={
@@ -52,6 +54,7 @@ class GeoCADModelTest(TestCase):
                 ],
             },
             insertion={"type": "Point", "coordinates": [12.523826, 41.90339]},
+            data={"Block": block.name, "X scale": 2, "attributes": {"Faz": "Baz"}},
         )
 
     @classmethod
@@ -64,10 +67,26 @@ class GeoCADModelTest(TestCase):
                 Path(file).unlink()
         except FileNotFoundError:
             pass
-        try:
-            path = Path(settings.MEDIA_ROOT).joinpath("uploads/djeocad/images/")
-            list = [e for e in path.iterdir() if e.is_file()]
-            for file in list:
-                Path(file).unlink()
-        except FileNotFoundError:
-            pass
+
+    def call_command(self, *args, **kwargs):
+        out = StringIO()
+        call_command(
+            "populate_block_field",
+            *args,
+            stdout=out,
+            stderr=StringIO(),
+            **kwargs,
+        )
+        return out.getvalue()
+
+    def test_command(self):
+        self.call_command()
+        block = Layer.objects.get(name="Bloke")
+        for ent in Entity.objects.all():
+            self.assertTrue("processed" in ent.data)
+            if "Block" in ent.data and ent.data["Block"] == "Bloke":
+                self.assertEqual(ent.block, block)
+                self.assertEqual(ent.xscale, 2)
+                for e in ent.related_data.all():
+                    self.assertEqual(e.key, "Faz")
+                    self.assertEqual(e.value, "Baz")
